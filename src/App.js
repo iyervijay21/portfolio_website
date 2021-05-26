da0367d908a580358827e59cea575d0ba5b99737
iyervijay21 <iyervignesh235@gmail.com>
2021-05-26 23:17:47 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Aboutme from "./components/Aboutme";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
+import Footer from "./components/Footer";
+import ProjectSection from "./components/ProjectSection";
+import RecommendationSection from "./components/RecommendationSection";
+import SkillsSection from "./components/SkillsSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Title />
+      <RecommendationSection />
+      <SkillsSection />
+      <ProjectSection />
+      <Aboutme />
+      <Footer />
     </div>
   );
 }
